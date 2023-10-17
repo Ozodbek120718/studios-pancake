80345e4219ac45891c880445638dbad284c2dd10
Sasha Raimov <cyberhub.python@gmail.com>
2023-10-17 10:41:06 +0500

    python_variables_multiple

---
+++ hello.py
@@ -0,0 +1,22 @@
+print('hello world');
+# privet vsem tem kto chitayet nash kod
+# if False :
+#     print('hi');
+
+print('no conditions block');
+
+x = 24; y = 'Ozodbek';
+
+"""
+    this is comment in triple quotes 
+    fgfd
+"""
+print(x, y, """dasdgdf""")
+z = '34';
+print(int(z) + 43);
+
+print(type(int(z)));
+
+age  = 11
+
+print(age, type(age));
