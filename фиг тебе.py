3aa02f6f1afbad9267b11a191a253f0a10168b5a
Ozodbek120718 <lol729586@gmail.com>
2023-10-17 10:30:23 +0500

    initial commit

---
+++ фиг тебе.py
@@ -0,0 +1,37 @@
+new_var = print('фиг тебе');
+if True  :
+    print('ИДИ НАФИГ')
+
+
+    print('ИДИ Уроки делаааай!!!!');
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!");
+
+
+x = 11
+y = "Ozodbek"
+print(x)
+print(y)
+
+#This is a comment.
+print("Ozodbek");
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+
+
+
+
+
+    
