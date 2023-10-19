d3bea396ade3b4959e7edd5baeec865e430e9c51
Sasha Raimov <cyberhub.python@gmail.com>
2023-10-19 09:55:17 +0500

    fix

--- hello.py
+++ hello.py
@@ -20,3 +20,39 @@ print(type(int(z)));
 age  = 11
 
 print(age, type(age));
+
+
+name, surname, age = 'Ozodbek', 'Mamayoqubov', 11
+
+print(name, surname, age);
+
+
+a = b = c= 5;
+
+print(a,b,c);
+
+names = ['Ozodbek', 'Odilbek', 'Sasha'];
+
+ozodbek, odilbek, _ = names;
+
+print(ozodbek, odilbek,);
+
+
+text = 'Hello, '
+
+print(text + name);
+
+print(str(6)+ 'asd');
+
+print(45, 54.5, 'str', [32,543,54], (3,23,4), {}, True, False);
+print(type(45))
+print(type(45.5))
+print(type('asd'))
+print(type([45,767]));
+print(type((34,564,)));
+print(type({'asd': 'asd'}));
+print(type(True));
+print(type(False));
+print(type(None));
+
+print(type(int('43d')));
