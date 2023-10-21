4aa640b7e547ac9322c1fb34c95530585864153c
Ozodbek120718 <lol729586@gmail.com>
2023-10-21 09:54:44 +0500

    Ozodbek/ 3 classwork

--- фиг тебе.py
+++ фиг тебе.py
@@ -1,37 +1,7 @@
-new_var = print('фиг тебе');
-if True  :
-    print('ИДИ НАФИГ')
+currentYear = 2023;
 
+InYear = 365;
 
-    print('ИДИ Уроки делаааай!!!!');
+age = int(input('Сколько вам лет вообще то пацухи? :'));
 
-if 5 > 2:
- print("Five is greater than two!") 
-if 5 > 2:
-        print("Five is greater than two!");
-
-
-x = 11
-y = "Ozodbek"
-print(x)
-print(y)
-
-#This is a comment.
-print("Ozodbek");
-
-x = 5
-y = "John"
-print(type(x))
-print(type(y))
-
-
-a = 4
-A = "Sally"
-#A will not overwrite a
-
-
-
-
-
-
-    
+print('ты прожил', age * InYear, ' дней');
