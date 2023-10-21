3c46f494c3aa1f6607190a275eb0f9d448e89ceb
Ozodbek120718 <lol729586@gmail.com>
2023-10-21 10:10:19 +0500

    Ozodbek/age detector

--- Ozodbek.py
+++ Ozodbek.py
@@ -2,6 +2,8 @@ currentYear = 2023;
 
 InYear = 365;
 
+inDay = 24;
+
 age = int(input('Сколько вам лет вообще то пацухи? :'));
 
-print('ты прожил', age * InYear, ' дней');
+print('ты прожил', InYear * inDay * age, ' часов');
