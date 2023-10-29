5d5f6ad7ed1a9aef4a0ebdb90a0f4e33cf63f4f0
Sasha Raimov <crazyysasha@gmail.com>
2023-10-29 14:57:44 +0500

    проверить все методы строки Fixes #3

---
+++ strings.py
@@ -0,0 +1,5 @@
+text = 'Privet Vsem';
+
+print(text, text.capitalize()) #  answer: делает самое первое слово заглавной
+print(text, text.casefold()); # answer: переобразовывает текст на прописные символы
+print(text, text.center(40)); # answer: Добавляет пробелы. конкретно то количество пробелов которые мы указываем в параметре
