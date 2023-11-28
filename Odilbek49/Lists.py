875f1128fab6205fc6aec7b2cd609882a6e4a89d
Odilbek <lezginodilbek@gmail.com>
2023-11-28 18:40:08 +0500

    Merge pull request #9 from Ozodbek120718:Odilbek49/issue6 апр

---
+++ Odilbek49/Lists.py
@@ -0,0 +1,18 @@
+Mylist = ['Kompyuter', 'laptop', 'telephone', 'google'];
+#Показывает Листа
+print(Mylist)
+
+#Показывает индексе
+print(Mylist[0])
+
+#Добавляет 
+Mylist.append("orange")
+print(Mylist)
+
+#Ремов убирает елемент
+Mylist.remove("laptop")
+print(Mylist)
+
+#Сортировка
+Mylist.sort()
+print(Mylist)
