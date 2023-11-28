0eeb38ddf5555cb89a5ac24809331cbd4a620bfa
Sasha Raimov <crazyysasha@gmail.com>
2023-11-28 18:30:37 +0500

    theory: lists Fixes #11

---
+++ crazyysasha/lists_theory.py
@@ -0,0 +1,13 @@
+
+# 1. определение
+items = ['televizor', 'telephone', 'mouse', 'laptop']
+print(items)
+
+# обращатся к элементам по индексу
+print(items[0]);
+
+# изменять порядок
+
+print(sorted(items));
+
+print(items);
